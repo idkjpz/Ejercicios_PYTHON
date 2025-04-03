7f08b1731313e4e85b1d7ddb809e8b9959ac7602
Juampi <jppaz2002@gmail.com>
2025-04-02 21:36:17 -0300

    Ejercico 3 resuelto

---
+++ Ejercicios_Basicos/algoritmo_basico3.py
@@ -0,0 +1,10 @@
+#Pedir el nombre y el sueldo, incrementarle un 10% e informar el aumento de su
+#sueldo para esa persona.
+
+incremento = 100
+
+numero_random = 3
+
+resultado = numero_random + (numero_random * incremento) / 100
+
+print(f"el numero {numero_random} incrementado en 100% es {resultado} ")
